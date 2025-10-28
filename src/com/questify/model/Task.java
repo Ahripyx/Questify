8b152c758766378ab0edd754a2c51202724e956f
Ahri <ahripyx@gmail.com>
2025-10-28 02:54:31 -0400

    Added task model

---
+++ src/com/questify/model/Task.java
@@ -0,0 +1,26 @@
+package com.questify.model;
+
+public class Task {
+	private String id;
+	private String title;
+	private boolean done;
+	
+	public Task(String id, String title, boolean done) {
+		this.id = id;
+		this.title = title;
+		this.done = done;
+	}
+	
+	public String getId() { return id; }
+	public String getTitle() { return title;}
+	public boolean isDone() { return done; }
+	
+	public void setTitle(String title) { this.title = title; }
+	public void setDone(boolean done) { this.done = done; }
+	
+	@Override
+	public String toString() {
+		return (done ? "[x] " : "[ ] ") + title;
+	}
+
+}
