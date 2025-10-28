9b501e74119502304ba6668565beadf661e15a49
Ahri <ahripyx@gmail.com>
2025-10-28 03:12:54 -0400

    Config utility model created

---
+++ src/com/questify/util/ConfigStore.java
@@ -0,0 +1,43 @@
+package com.questify.util;
+
+import java.nio.file.*;
+import java.util.Properties;
+import java.io.*;
+
+public class ConfigStore {
+	private final Path cfgFile;
+	
+	public ConfigStore(Path cfgFile) {
+		this.cfgFile = cfgFile;
+	}
+	
+	public boolean isPrivacyAccepted() {
+		Properties p = load();
+		return Boolean.parseBoolean(p.getProperty("privacyAccepted", "false"));
+	}
+	
+	public void setPrivacyAccepted(boolean v) {
+		Properties p = load();
+		p.setProperty("privacyAccepted", Boolean.toString(v));
+		save(p);
+	}
+	
+	private Properties load() {
+		Properties p = new Properties();
+		if (Files.exists(cfgFile)) {
+			try (InputStream in = Files.newInputStream(cfgFile)){
+				p.load(in);
+			} catch (IOException e) { /* Ignore */}
+		}
+		return p;
+	}
+	
+	private void save(Properties p) {
+        try {
+            Files.createDirectories(cfgFile.getParent());
+            try (OutputStream out = Files.newOutputStream(cfgFile)) {
+                p.store(out, "Questify config");
+            }
+        } catch (IOException e) { e.printStackTrace(); }
+    }
+}
